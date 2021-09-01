9896c7ce5ba7570b663ee0f27d7aaa1409d35b6b
AliseBaumane <alise.adamsone@gmail.com>
2021-09-01 20:53:25 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+
+package com.company;
+
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner Scanner = new Scanner(System.in);
+        System.out.println("Enter numbers with comma");
+        double number=Scanner.nextDouble();
+        int digit=(int) number;
+        System.out.println("number " +digit);
+
+        //String Number = Scanner.nextLine();
+
+
+    }
+}
